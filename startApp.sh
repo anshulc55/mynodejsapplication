16ab393f0aa5e4b15f805c10eca8c6970b065b82
Anshul Chauhan <anshulc55@gmail.com>
2018-10-05 20:35:56 +0530

    Chnage in Port

--- startApp.sh
+++ startApp.sh
@@ -4,6 +4,6 @@ export DB_PRD_HOST=dellstore2.cohobyukovto.ap-south-1.rds.amazonaws.com
 export DB_PRD_USER=dellstoreaws
 export DB_PRD_PASS=dellstoreaws
 export NODE_HOST=localhost
-export NODE_PORT=8080
+export NODE_PORT=3000
 node /myapp/index.js&
 exit 0
